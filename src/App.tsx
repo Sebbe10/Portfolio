3f24ecf3219d4069645c240c7f35c56d34a2bc63
Your Name <sebastian.bjelkstam@gmail.com>
2023-05-02 16:07:08 +0200

    Portfolio

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,13 @@ import logo from "./logo.svg";
 import "./App.css";
 import { Outlet } from "react-router-dom";
 import { Nav } from "./Nav/nav";
-import { HashRouter as Router } from "react-router-dom";
 
 function App() {
   return (
-    <Router>
-      <div className="App">
-        <Nav />
-        <Outlet></Outlet>
-      </div>
-    </Router>
+    <div className="App">
+      <Nav />
+      <Outlet></Outlet>
+    </div>
   );
 }
 
